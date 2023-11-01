7046b0787e19f92065c1e61a18f8424a14dab1c9
조익 <dream-ik89@naver.com>
2023-11-01 13:01:32 +0900

    Initial commit - 초기 커밋

---
+++ src/test/java/com/backkeesun/inflearnrestapi/BackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backkeesun.inflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
