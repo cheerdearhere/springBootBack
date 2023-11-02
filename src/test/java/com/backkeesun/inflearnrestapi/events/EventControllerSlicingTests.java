c310773f3d3e293a3806ec7ca66d946174bc0e20
조익 <dream-ik89@naver.com>
2023-11-02 13:14:28 +0900

    test: slice test 종료, spring mock test

--- src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerSlicingTests.java
+++ src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerSlicingTests.java
@@ -5,9 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -16,22 +14,23 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Slicing test로는 제대로 영속성 처리를 체크할 수 없으므로 test를 더 진행할 수 없다
- * WebMvc 계층 테스트를 spring test로 변경
+ * Mocking 테스트는 여기까지만 진행하고 Stop
  */
-@SpringBootTest
-@AutoConfigureMockMvc // spring내에서 mock사용
-class EventControllerTests {
+@WebMvcTest // MockMvc를 주입받아 사용
+class EventControllerSlicingTests {
     @Autowired
-    MockMvc mockMvc; //AutoConfigureMockMvc로 사용 가능
+    MockMvc mockMvc; //웹과 같은 환경으로 테스트(계층별 테스트: slicing test)
     @Autowired
     ObjectMapper objectMapper;// json으로 변환
-    //repository도 포함되므로 Mockito는 필요 x
+    @MockBean // 이 bean을 MockBean으로 만들어 줌
+    EventRepository eventRepository;//repository는 webBean이 아니어서 주입되지 않음
+
     @Test
     void createEvent() throws Exception {
         //given
@@ -49,9 +48,13 @@ class EventControllerTests {
                 .location("강남역 D2 스타터 팩토리")
                 .free(true)//입력되서는 안될 값 체크용
                 .offline(false)
-                .eventStatus(EventStatus.PUBLISHED)
                 .build();
         //when & then
+            //test의 경우 id값을 생성하지 않으므로 nullPointException 발생. 이를 대비한 처리
+        Mockito.when(
+                eventRepository
+                    .save(event))
+                    .thenReturn(event);
         mockMvc.perform(
                     post("/api/events")//HTTPRequestServlet Method
                         .contentType(MediaType.APPLICATION_JSON) // request 구체적 구현
@@ -63,11 +66,10 @@ class EventControllerTests {
                 .andExpect(jsonPath("id").exists())//연속적으로 데이터 확인 가능
                 .andExpect(header().exists(HttpHeaders.LOCATION))
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                //입력된 값에 대한 제한 체크
-                .andExpect(jsonPath("id").value(Matchers.not(10)))//id 값은 generate 처리된 값
-                .andExpect(jsonPath("free").value(Matchers.not(true)))//default값 확인
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
-//                .andExpect(jsonPath("offline").value(Matchers.not(false))) 아직 내부 연산 처리 안됨
+                //입력된 값에 대한 제한 체크(웹 단계의 슬라이싱 테스트에서는 제대로 generate되는지 확인 불가)
+//                .andExpect(jsonPath("id").value(Matchers.not(10)))//id 값은 generate 처리된 값
+//                .andExpect(jsonPath("free").value(Matchers.not(true)))
+//                .andExpect(jsonPath("offline").value(Matchers.not(false)))
         ;
         /* TDD는 보통 데이터 3개 정도를 넣고 진행 */
     }
