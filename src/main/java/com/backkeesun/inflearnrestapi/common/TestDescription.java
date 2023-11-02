ee638fd0bee2ce7fe54d5c9a05e3c81005c47db2
조익 <dream-ik89@naver.com>
2023-11-02 16:33:55 +0900

    chore: 주석 추가(@DisplayName) annotation 복붙

--- src/main/java/com/backkeesun/inflearnrestapi/common/TestDescription.java
+++ src/main/java/com/backkeesun/inflearnrestapi/common/TestDescription.java
@@ -1,9 +1,20 @@
 package com.backkeesun.inflearnrestapi.common;
 
+import org.aspectj.weaver.ast.Test;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+/**
+ * {@code @DisplayName} is used to declare a {@linkplain #value custom display
+ * name} for the annotated test class or test method.
+ *
+ * <p>Display names are typically used for test reporting in IDEs and build
+ * tools and may contain spaces, special characters, and even emoji.
+ *
+ * @see Test
+ */
 
 @Target(ElementType.METHOD) // 대상
 @Retention(RetentionPolicy.SOURCE) // life cycle
