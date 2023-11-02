4d9048cdac7204dbc2f2fcfc33d5e12b8fa6850c
조익 <dream-ik89@naver.com>
2023-11-02 14:43:46 +0900

    test: event Dto validation(check wrong data)
    - test 코드 작성
    - junit5의 @DisplayName으로 테스트코드 표시 명칭 변경

--- src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package com.backkeesun.inflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,7 @@ class EventControllerTests {
     ObjectMapper objectMapper;// json으로 변환
     //repository도 포함되므로 Mockito는 필요 x
     @Test
+    @DisplayName(value = "정상 처리된 경우 확인")
     void createEvent() throws Exception {
         //given
         EventDto event = EventDto.builder()
@@ -42,7 +44,7 @@ class EventControllerTests {
                 .beginEnrollmentDateTime(LocalDateTime.of(2018,11,12,13,21))
                 .closeEnrollmentDateTime(LocalDateTime.of(2018,12,30,11,12))
                 .beginEventDateTime(LocalDateTime.of(2018, 11, 14,10,5))
-                .endEventDateTime(LocalDateTime.of(2018,12,1,23,00))
+                .endEventDateTime(LocalDateTime.of(2019,12,1,23,1))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -71,6 +73,7 @@ class EventControllerTests {
         /* TDD는 보통 데이터 3개 정도를 넣고 진행 */
     }
     @Test
+    @DisplayName(value = "불필요한 값이 입력 됐을때 response code 체크")
     void createEvent_BadRequest() throws Exception {
         //given
         Event event = Event.builder()
@@ -80,7 +83,7 @@ class EventControllerTests {
                 .beginEnrollmentDateTime(LocalDateTime.of(2018,11,12,13,21))
                 .closeEnrollmentDateTime(LocalDateTime.of(2018,12,30,11,12))
                 .beginEventDateTime(LocalDateTime.of(2018, 11, 14,10,5))
-                .endEventDateTime(LocalDateTime.of(2018,12,1,23,00))
+                .endEventDateTime(LocalDateTime.of(2018,12,1,23,1))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -100,6 +103,7 @@ class EventControllerTests {
         ;
     }
     @Test
+    @DisplayName(value = "빈 값이 입력 됐을때 response code 체크")
     void createEvent_BadRequest_EmptyInput() throws Exception {
         //given
         EventDto eventDto = EventDto.builder().build();
@@ -112,4 +116,30 @@ class EventControllerTests {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    @DisplayName(value = "잘못된 값이 입력 됐을때 response code 체크")
+    void createEvent_BadRequest_WrongData() throws Exception{
+        //given
+        EventDto eventDto = EventDto.builder()
+                .name("spring")
+                .description("RestAPI Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,12,13,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,12,30,11,12))
+                //끝나는 날짜가 시작날짜보다 빠를때 테스트
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 14,10,5))
+                .endEventDateTime(LocalDateTime.of(2018,11,1,23,1))
+                //base > max 테스트
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타터 팩토리")
+                .build();
+        //when then
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(eventDto))
+                )
+                .andExpect(status().isBadRequest());
+    }
 }
