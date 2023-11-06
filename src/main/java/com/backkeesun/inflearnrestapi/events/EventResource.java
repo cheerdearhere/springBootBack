1235f62f80182e86a636677c95b941651997392f
조익 <dream-ik89@naver.com>
2023-11-06 10:49:42 +0900

    feat: event 컨트롤러에 HATEOAS 적용

---
+++ src/main/java/com/backkeesun/inflearnrestapi/events/EventResource.java
@@ -0,0 +1,37 @@
+package com.backkeesun.inflearnrestapi.events;
+
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import java.util.Arrays;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class EventResource extends EntityModel<Event> {
+    public EventResource(Event event, Link... links){
+        super(event, Arrays.asList(links));
+        WebMvcLinkBuilder resourceBuilder = linkTo(EventController.class).slash(event.getId());
+        //new Link("url")도 가능:         add(new Link("http://localhost:8181/api/events/"+event.getId()));
+        //데이터가 변경되면 하나하나 직접 변경해야흐므로 아래를 권장
+        add(resourceBuilder.withSelfRel());
+        add(resourceBuilder.withRel("query-events"));
+        add(resourceBuilder.withRel("update-event"));
+    }
+}
+/**
+ * 1번 방법: RepresentationModel 사용
+ */
+//import com.fasterxml.jackson.annotation.JsonUnwrapped;
+//import org.springframework.hateoas.RepresentationModel;
+//public class EventResource extends RepresentationModel {
+//    @JsonUnwrapped
+//    private Event event;
+//    public EventResource (Event event){
+//        this.event = event;
+//    }
+//    public Event getEvent() {
+//        return event;
+//    }
+//}
