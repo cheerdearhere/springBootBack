f758b7112d35f51ad0ce1388feefd8f1d24d969b
조익 <dream-ik89@naver.com>
2023-11-06 14:53:49 +0900

    test: request, response의 헤더와 필드의 docs snippet 만들기

--- src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/backkeesun/inflearnrestapi/events/EventControllerTests.java
@@ -17,11 +17,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -210,7 +215,7 @@ class EventControllerTests {
 
     @Test
     @DisplayName(value = "spring rest docs처리 확인")
-    void test()throws Exception{
+    void restDocsBasic()throws Exception{
         EventDto eventDto = inputDataObject();
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -222,11 +227,94 @@ class EventControllerTests {
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE))
                 .andExpect(jsonPath("id").exists())
                 .andExpect(jsonPath("_links.self").exists())
-//                .andExpect(jsonPath("_link.profile").exists()) 아직 작성 x
                 .andExpect(jsonPath("_links.query-events").exists())
                 .andExpect(jsonPath("_links.update-event").exists())
                 .andDo(document("create_event"))
                 ;
+    }
+
+
+    /**
+     * API 문서 조각 만들기
+     *  요청 본문 문서화
+     *  응답 본문 문서화
+     *  링크 문서화
+     *    * self, query, update
+     *    * profile 링크(문서 완성 후)
+     *  요청 헤더 문서화
+     *  요청 필드 문서화
+     *  응답 헤더 문서화
+     *  응답 필드 문서화
+     * @throws Exception
+     */
+    @Test
+    @DisplayName(value = "spring rest docs 문서 조각(스니펫) 만들기")
+    void restDocsField() throws Exception{
+        EventDto eventDto = inputDataObject();
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(eventDto))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("_links.self").exists())
+//                .andExpect(jsonPath("_link.profile").exists()) 아직 작성 x
+                .andExpect(jsonPath("_links.query-events").exists())
+                .andExpect(jsonPath("_links.update-event").exists())
+                //이곳에서 작성
+                .andDo(document("create_event",
+                        links(
+                                linkWithRel("self").description("Link to self"),
+                                linkWithRel("query-events").description("Link to query events"),
+                                linkWithRel("update-event").description("Link to update an existing event")
+//                              ,  linkWithRel("profile").description("Link to profile page")
+                        ),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.ACCEPT).description("header:"+MediaTypes.HAL_JSON_VALUE),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("contentType: "+MediaType.APPLICATION_JSON)
+                        ),
+                        requestFields(
+                                fieldWithPath("name").description("event name"),
+                                fieldWithPath("description").description("information of new event"),
+                                fieldWithPath("beginEnrollmentDateTime").description("date and time of begin of new event"),
+                                fieldWithPath("closeEnrollmentDateTime").description("date and time of expire of new event"),
+                                fieldWithPath("beginEventDateTime").description("date and time of start of event"),
+                                fieldWithPath("endEventDateTime").description("date and time of finish of event"),
+                                fieldWithPath("location").description("address of event, null is online event"),
+                                fieldWithPath("basePrice").description("minimum price to join event"),
+                                fieldWithPath("maxPrice").description("maximum price to join event"),
+                                fieldWithPath("limitOfEnrollment").description("maximum user number")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("address of event"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("contentType"+MediaTypes.HAL_JSON_VALUE)
+                        ),
+                        //
+                        responseFields(
+                                fieldWithPath("id").description("event id"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("event name"),
+                                fieldWithPath("description").description("information of new event"),
+                                fieldWithPath("beginEnrollmentDateTime").description("date and time of begin of new event"),
+                                fieldWithPath("closeEnrollmentDateTime").description("date and time of expire of new event"),
+                                fieldWithPath("beginEventDateTime").description("date and time of start of event"),
+                                fieldWithPath("endEventDateTime").description("date and time of finish of event"),
+                                fieldWithPath("location").description("address of event, null is online event"),
+                                fieldWithPath("basePrice").description("minimum price to join event"),
+                                fieldWithPath("maxPrice").description("maximum price to join event"),
+                                fieldWithPath("limitOfEnrollment").description("maximum user number"),
+                                fieldWithPath("offline").description("has location ? true : false"),
+                                fieldWithPath("free").description("has basePrice or maxPrice ? false : true"),
+                                fieldWithPath("eventStatus").description("event's current status"),
+                                //optional fields
+                                fieldWithPath("_links.self.href").type(JsonFieldType.STRING).description("my href").optional(),
+                                fieldWithPath("_links.query-events.href").type(JsonFieldType.STRING).description("my href").optional(),
+                                fieldWithPath("_links.update-event.href").type(JsonFieldType.STRING).description("my href").optional()
+                        )
+
+                ));
     }
 
     private static EventDto inputDataObject() {
