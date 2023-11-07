ccd00a8559cbf6733aa3adb0e775207b20833f7a
조익 <dream-ik89@naver.com>
2023-11-07 14:56:52 +0900

    feat: api index 제공 API

---
+++ src/main/java/com/backkeesun/inflearnrestapi/index/IndexController.java
@@ -0,0 +1,25 @@
+package com.backkeesun.inflearnrestapi.index;
+
+import com.backkeesun.inflearnrestapi.events.Event;
+import com.backkeesun.inflearnrestapi.events.EventController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class IndexController {
+    @GetMapping
+    public RepresentationModel index(){
+        var index= new RepresentationModel();
+        index.add(linkTo(EventController.class).withRel("events"));
+        return index;
+    }
+}
