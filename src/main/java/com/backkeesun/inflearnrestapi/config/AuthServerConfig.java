33f05ca9a3c31a9ae53e96368bd45ca1808f2490
조익 <dream-ik89@naver.com>
2023-11-10 16:51:46 +0900

    feat: 토큰 발행용 처리(/oauth/token)

---
+++ src/main/java/com/backkeesun/inflearnrestapi/config/AuthServerConfig.java
@@ -0,0 +1,49 @@
+package com.backkeesun.inflearnrestapi.config;
+
+import com.backkeesun.inflearnrestapi.account.AccountRole;
+import com.backkeesun.inflearnrestapi.account.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+@RequiredArgsConstructor
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {//"/oauth/token" url은 자동처리
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final AccountService accountService;
+    private final TokenStore tokenStore;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.passwordEncoder(passwordEncoder);//client_secret을 처리할때
+    }
+    private final int TOKEN_VALID_TIME = 30 * 60 * 60;
+    @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()//test용 실무에서는 .jdbc로 db처리
+                    .withClient("myClientId")
+                    .authorizedGrantTypes("password","refresh_token")
+                    .secret(passwordEncoder.encode("sec"))
+                    .scopes("read","write")
+                    //토큰 만료시간(sec)
+//                    .accessTokenValiditySeconds(TOKEN_VALID_TIME)
+//                    .refreshTokenValiditySeconds(TOKEN_VALID_TIME)
+            ;
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager)//authentication처리때 사용할 이전에 등록한 bean 등록
+                    .userDetailsService(accountService)
+                    .tokenStore(tokenStore);
+        }
+}
